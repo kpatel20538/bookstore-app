2e8db4df8de9f7c4c3356be62b17279c6f592dd5
Kishan Patel <kpatel16@tamu.edu>
2020-08-02 04:32:51 -0500

    Scafollding Home Page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,24 +1,64 @@
 import React from "react";
-import { Hero, Title } from "rbx";
+import {
+  Hero,
+  Title,
+  Column,
+  Navbar,
+  Block,
+  Notification,
+  Container,
+  Section,
+} from "rbx";
 
-import Ratings from "../../components/Ratings";
+const HeroSection = () => {
+  return (
+    <Hero color="primary" size="medium" textAlign="centered">
+      <Hero.Body>
+        <Title>Home Page</Title>
+      </Hero.Body>
+    </Hero>
+  );
+}
+
+
+const SearchBar = () => <Navbar color="light" />
+const CategoryBar = () => <Navbar color="dark" />;
+const BestSellerSection = () => <Block><Notification color="primary"/></Block>
+const CategoryGallery = () => <Notification color="dark" />;
+const RecommendationsSection = () => (
+  <Section>
+    <Notification color="light" />
+  </Section>
+);
 
 const Home = () => {
   return (
     <>
-      <Hero color="primary" size="medium" textAlign="centered">
-        <Hero.Body>
-          <Title>Home Page</Title>
-        </Hero.Body>
-      </Hero>
-      <Ratings rating={3} />
-      <br />
-      <Ratings rating={2} />
-      <br />
-      <Ratings rating={4.5} />
-      <br />
-      <Ratings rating={5} />
-      <br />
+      <HeroSection />
+      <SearchBar />
+      <CategoryBar />
+      <Container>
+        <BestSellerSection />
+        <Column.Group>
+          <Column size="one-third">
+            <Notification color="primary">
+              <Title>Your Past Orders</Title>
+            </Notification>
+          </Column>
+          <Column size="one-third">
+            <Notification color="dark">
+              <Title>Your Shopping Cart</Title>
+            </Notification>
+          </Column>
+          <Column size="one-third">
+            <Notification color="light">
+              <Title>Best Sellers</Title>
+            </Notification>
+          </Column>
+        </Column.Group>
+        <CategoryGallery />
+        <RecommendationsSection />
+      </Container>
     </>
   );
 };
